ea860c508f9ecada2bcad3a8092ab6cfb8cbdf11
ArshiyaASM <arshiyaasemaniw@gmail.com>
2024-10-11 15:33:17 +0330

    first version

---
+++ project12.js
@@ -0,0 +1,30 @@
+const open_bar = document.querySelector("#open_bar");
+const menu_bar = document.querySelector(".menu_bar");
+
+
+open_bar.addEventListener("click" , e => {
+    if(!e.currentTarget.classList.contains("close_bar")){
+        menu_bar.style.display = "block";
+        e.currentTarget.src = "../image/icon-close.svg";
+        e.currentTarget.classList.toggle("close_bar");
+    }
+
+    else {
+        menu_bar.style.display = "none";
+        e.currentTarget.src = "../image/icon-hamburger.svg";
+        e.currentTarget.classList.toggle("close_bar");
+    }
+})
+
+
+window.addEventListener("resize" , e => {
+    let width = window.innerWidth
+    if(width > 768){
+        if(open_bar.classList.contains("close_bar")){
+            menu_bar.style.display = "none";
+            open_bar.src = "../image/icon-hamburger.svg";
+            open_bar.classList.toggle("close_bar");
+        }   
+        
+    }
+})
